5db2149ec6c71df80a7a2cfbe626fa417553cedc
Burak Öztaş <143541613+BurakOztas94@users.noreply.github.com>
2024-05-25 00:01:12 +0300

    Project initiation

---
+++ src/main/java/com/commercial/mobileCommercial/MobileCommercialApplication.java
@@ -0,0 +1,13 @@
+package com.commercial.mobileCommercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileCommercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileCommercialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commercial/mobileCommercial/MobileCommercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commercial.mobileCommercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileCommercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
